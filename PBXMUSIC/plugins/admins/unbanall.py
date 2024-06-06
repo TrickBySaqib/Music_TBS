0e92afe58900007c432b997288fb9a88bde9a166
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-06-07 01:14:19 +0530

    Add files via upload

--- PBXMUSIC/plugins/admins/unbanall.py
+++ PBXMUSIC/plugins/admins/unbanall.py
@@ -1,9 +1,10 @@
 from PBXMUSIC import app
+from config import OWNER_ID
 from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from PBXMUSIC.utils.bad_ban import admin_filter
 
 BOT_ID = app.id
-
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
@@ -13,9 +14,7 @@ async def unban_all(_, msg):
     bot_permission = bot.privileges.can_restrict_members == True
     if bot_permission:
         banned_users = []
-        async for m in app.get_chat_members(
-            chat_id, filter=enums.ChatMembersFilter.BANNED
-        ):
+        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BANNED):
             banned_users.append(m.user.id)
             try:
                 await app.unban_chat_member(chat_id, banned_users[x])
@@ -24,14 +23,10 @@ async def unban_all(_, msg):
             except Exception:
                 pass
     else:
-        await msg.reply_text(
-            "ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs"
-        )
-
+        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")
 
 @app.on_callback_query(filters.regex("^stop$"))
 async def stop_callback(_, query):
     await query.message.delete()
-
 
 ###
