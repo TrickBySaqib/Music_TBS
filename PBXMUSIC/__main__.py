ee940768359ffdc2567c94d2826c3da23a3c1231
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-06-07 00:56:46 +0530

    Add files via upload

--- PBXMUSIC/__main__.py
+++ PBXMUSIC/__main__.py
@@ -1,16 +1,19 @@
 import asyncio
 import importlib
-
+from sys import argv
 from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
 from PBXMUSIC import LOGGER, app, userbot
 from PBXMUSIC.core.call import PBX
 from PBXMUSIC.misc import sudo
 from PBXMUSIC.plugins import ALL_MODULES
-from PBXMUSIC.plugins.tools.clone import restart_bots
 from PBXMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+from PBXMUSIC.plugins.tools.clone import restart_bots
+
 
 
 async def init():
