4e85055135b64d38dcedf0327b7c82d9bfd092d4
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-06-07 01:11:34 +0530

    Add files via upload

--- PBXMUSIC/plugins/misc/info.py
+++ PBXMUSIC/plugins/misc/info.py
@@ -1,15 +1,23 @@
+import asyncio, os, time, aiohttp
+from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
+from asyncio import sleep
 from PBXMUSIC import app
-from pyrogram import filters, enums
+from pyrogram import filters, Client, enums
+from pyrogram.enums import ParseMode
+from pyrogram.types import *
 from typing import Union, Optional
+
 
 
 # --------------------------------------------------------------------------------- #
 
 
 get_font = lambda font_size, font_path: ImageFont.truetype(font_path, font_size)
-resize_text = lambda text_size, text: (
-    (text[:text_size] + "...").upper() if len(text) > text_size else text.upper()
+resize_text = (
+    lambda text_size, text: (text[:text_size] + "...").upper()
+    if len(text) > text_size
+    else text.upper()
 )
 
 # --------------------------------------------------------------------------------- #
@@ -18,8 +26,8 @@ resize_text = lambda text_size, text: (
 async def get_userinfo_img(
     bg_path: str,
     font_path: str,
-    user_id: Union[int, str],
-    profile_path: Optional[str] = None,
+    user_id: Union[int, str],    
+    profile_path: Optional[str] = None
 ):
     bg = Image.open(bg_path)
 
@@ -43,10 +51,11 @@ async def get_userinfo_img(
         fill=(255, 255, 255),
     )
 
+
     path = f"./userinfo_img_{user_id}.png"
     bg.save(path)
     return path
-
+   
 
 # --------------------------------------------------------------------------------- #
 
@@ -74,37 +83,31 @@ INFO_TEXT = """**
 
 # --------------------------------------------------------------------------------- #
 
-
 async def userstatus(user_id):
-    try:
-        user = await app.get_users(user_id)
-        x = user.status
-        if x == enums.UserStatus.RECENTLY:
-            return "Recently."
-        elif x == enums.UserStatus.LAST_WEEK:
-            return "Last week."
-        elif x == enums.UserStatus.LONG_AGO:
-            return "Long time ago."
-        elif x == enums.UserStatus.OFFLINE:
-            return "Offline."
-        elif x == enums.UserStatus.ONLINE:
-            return "Online."
-    except:
+   try:
+      user = await app.get_users(user_id)
+      x = user.status
+      if x == enums.UserStatus.RECENTLY:
+         return "Recently."
+      elif x == enums.UserStatus.LAST_WEEK:
+          return "Last week."
+      elif x == enums.UserStatus.LONG_AGO:
+          return "Long time ago."
+      elif x == enums.UserStatus.OFFLINE:
+          return "Offline."
+      elif x == enums.UserStatus.ONLINE:
+         return "Online."
+   except:
         return "**sᴏᴍᴇᴛʜɪɴɢ ᴡʀᴏɴɢ ʜᴀᴘᴘᴇɴᴇᴅ !**"
-
+    
 
 # --------------------------------------------------------------------------------- #
 
-
-@app.on_message(
-    filters.command(
-        ["info", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]
-    )
-)
+@app.on_message(filters.command(["info", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def userinfo(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
-
+    
     if not message.reply_to_message and len(message.command) == 2:
         try:
             user_id = message.text.split(None, 1)[1]
@@ -113,7 +116,7 @@ async def userinfo(_, message):
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
-            first_name = user_info.first_name
+            first_name = user_info.first_name 
             last_name = user_info.last_name if user_info.last_name else "No last name"
             username = user_info.username if user_info.username else "No Username"
             mention = user.mention
@@ -125,17 +128,11 @@ async def userinfo(_, message):
                 user_id=user_id,
                 profile_path=photo,
             )
-            await app.send_photo(
-                chat_id,
-                photo=welcome_photo,
-                caption=INFO_TEXT.format(
-                    id, first_name, last_name, username, mention, status, dc_id, bio
-                ),
-                reply_to_message_id=message.id,
-            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, first_name, last_name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
         except Exception as e:
-            await message.reply_text(str(e))
-
+            await message.reply_text(str(e))        
+      
     elif not message.reply_to_message:
         try:
             user_info = await app.get_chat(user_id)
@@ -143,7 +140,7 @@ async def userinfo(_, message):
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
-            first_name = user_info.first_name
+            first_name = user_info.first_name 
             last_name = user_info.last_name if user_info.last_name else "No last name"
             username = user_info.username if user_info.username else "No Username"
             mention = user.mention
@@ -155,17 +152,12 @@ async def userinfo(_, message):
                 user_id=user_id,
                 profile_path=photo,
             )
-            await app.send_photo(
-                chat_id,
-                photo=welcome_photo,
-                caption=INFO_TEXT.format(
-                    id, first_name, last_name, username, mention, status, dc_id, bio
-                ),
-                reply_to_message_id=message.id,
-            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, first_name, last_name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
         except Exception as e:
             await message.reply_name(str(e))
 
+            
     elif message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         try:
@@ -174,27 +166,21 @@ async def userinfo(_, message):
             status = await userstatus(user.id)
             id = user_info.id
             dc_id = user.dc_id
-            first_name = user_info.first_name
+            first_name = user_info.first_name 
             last_name = user_info.last_name if user_info.last_name else "No last name"
             username = user_info.username if user_info.username else "No Username"
             mention = user.mention
             bio = user_info.bio if user_info.bio else "No bio set"
-            photo = await app.download_media(
-                message.reply_to_message.from_user.photo.big_file_id
-            )
+            photo = await app.download_media(message.reply_to_message.from_user.photo.big_file_id)
             welcome_photo = await get_userinfo_img(
                 bg_path=bg_path,
                 font_path=font_path,
                 user_id=user_id,
                 profile_path=photo,
             )
-            await app.send_photo(
-                chat_id,
-                photo=welcome_photo,
-                caption=INFO_TEXT.format(
-                    id, first_name, last_name, username, mention, status, dc_id, bio
-                ),
-                reply_to_message_id=message.id,
-            )
+            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
+                id, first_name, last_name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
         except Exception as e:
             await message.reply_text(str(e))
+
+
